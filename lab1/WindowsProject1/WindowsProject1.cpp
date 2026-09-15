cbadc56b8c64e9e3871e728b6b1c0effee7a3629
alexbigachov <alexbigachov@gmail.com>
2026-09-15 21:08:45 +0300

    Виправлено папки!

---
+++ lab1/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,178 @@
+﻿// WindowsProject1.cpp : Defines the entry point for the application.
+#include "framework.h"
+#include "WindowsProject1.h"
+#include <string>
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst; // current instance
+WCHAR szTitle[MAX_LOADSTRING]; // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	// Ім'я класу вікна: "Bondarchuk, Bohachov"
+    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"Bondarchuk, Bohachov");
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance(hInstance, nCmdShow))
+    {return FALSE;}
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+//
+//  FUNCTION: MyRegisterClass()
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    // Стиль класу за варіантом:
+    // CS_NOCLOSE - забороняє команду "закрити"
+    // CS_HREDRAW - перемальовування вікна при зміні горизонтального розміру
+    // CS_VREDRAW - перемальовування вікна при зміні вертикального розміру
+    wcex.style = CS_NOCLOSE | CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION); // тип іконки за варіантом
+    wcex.hCursor = LoadCursor(nullptr, IDC_HELP); // тип курсора за варіантом
+    wcex.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(190, 190, 190)); // світло-сірий фон
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//   PURPOSE: Saves instance handle and creates main window
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Store instance handle in our global variable
+
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    // Позиція: по лівому краю екрана (x = 0), по вертикалі — по центру екрана
+    // Розмір: ширина 500, висота 200
+    int x = 0;
+    int y = (screenHeight - 200) / 2;
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        x, y, 500, 200, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {return FALSE;}
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//  PURPOSE: Processes messages for the main window.
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        RECT rect;
+        GetClientRect(hWnd, &rect);
+
+        // Отримуємо метрики шрифту, встановленого в контексті пристрою
+        TEXTMETRIC tm;
+        GetTextMetrics(hdc, &tm);
+
+        int fontHeight = tm.tmHeight;             // висота шрифта
+        int lineSpacing = tm.tmExternalLeading;    // міжрядковий відступ
+
+        std::wstring text = L"Висота шрифту: " + std::to_wstring(fontHeight) + L" пікс.\n" +
+            L"Міжрядкова відстань: " + std::to_wstring(lineSpacing) + L" пікс.";
+
+        DrawText(hdc, text.c_str(), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ lab2/WindowsProject2/WindowsProject2.cpp
@@ -0,0 +1,139 @@
+﻿#include "framework.h"
+#include "WindowsProject2.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобал змінні
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+// Змінні для збереження поточних розмірів клієнтської області вікна!
+int clientWidth = 500;
+int clientHeight = 300;
+
+// Константний масив, що задає бітове зображення (Рисунок: 16x16 пікселів)
+// Кожні 2 байти (16 біт) представляють один рядок пікселів. 1 - білий, 0 - чорний
+const BYTE bmpImageBits[32] = {
+    0x03, 0xC0, // 00000011 11000000
+    0x0F, 0xF0, // 00001111 11110000
+    0x1F, 0xF8, // 00011111 11111000
+    0x3F, 0xFC, // 00111111 11111100
+    0x33, 0xCC, // 00110011 11001100 (очі)
+    0x33, 0xCC, // 00110011 11001100
+    0x3F, 0xFC, // 00111111 11111100
+    0x3F, 0xFC, // 00111111 11111100
+    0x3C, 0x3C, // 00111100 00111100 (рот)
+    0x3E, 0x7C, // 00111110 01111100
+    0x3F, 0xFC, // 00111111 11111100
+    0x1F, 0xF8, // 00011111 11111000
+    0x0F, 0xF0, // 00001111 11110000
+    0x03, 0xC0, // 00000011 11000000
+    0x00, 0x00, // 00000000 00000000
+    0x00, 0x00  // 00000000 00000000
+};
+
+// Попередні оголошення функцій
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"BondarchukBohachov_Lab2");
+
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow))
+    {return FALSE;}
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT2));
+    MSG msg;
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW; // Перемальовування при зміні розмірів
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr; // Вимикаємо меню для чистоти вікна
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance;
+    HWND hWnd = CreateWindowW(szWindowClass, L"Лабораторна 2 - Варіант 7", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, clientWidth, clientHeight, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd)
+    {return FALSE;}
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_SIZE:
+        // Отримуємо нові розміри вікна при розтягуванні/стисканні
+        clientWidth = LOWORD(lParam);
+        clientHeight = HIWORD(lParam);
+
+        // Сповіщаємо систему, що вікно треба перемалювати
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        // 1. Створюємо бітове зображення з нашого константного масиву
+        // Параметри: ширина (16), висота (16), ксть кольорових площин (1), біт на піксель (1 - монохром), масив даних
+        HBITMAP hBitmap = CreateBitmap(16, 16, 1, 1, bmpImageBits);
+        // 2. Створюю сумісний контекст пристрою (в пам'яті), щоб завантажити туди малюнок
+        HDC hdcMem = CreateCompatibleDC(hdc);
+        // 3. Робимо наше зображення поточним для контексту пам'яті
+        HBITMAP hOldBitmap = (HBITMAP)SelectObject(hdcMem, hBitmap);
+        // 4. Виводимо зображення на екран з масштабуванням.
+        // Функція StretchBlt розтягує малюнок 16x16 до поточних розмірів вікна (clientWidth, clientHeight)
+        StretchBlt(hdc, 0, 0, clientWidth, clientHeight, hdcMem, 0, 0, 16, 16, SRCCOPY);
+        // 5. Очищення ресурсів GDI, щоб не було витоку пам'яті
+        SelectObject(hdcMem, hOldBitmap);
+        DeleteDC(hdcMem);
+        DeleteObject(hBitmap);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
